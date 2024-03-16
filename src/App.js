c9219bd9de53859019a877948293f20aa2136cc3
Chris <who7708@163.com>
2024-03-17 01:37:46 +0800

    组件

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import "./styles.css";
-
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//     </div>
-//   );
-// }
-
-function Greeting({ name }) {
-  return <h1>Hello {name}</h1>;
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
 }
 
-export default function App() {
-  return <Greeting name="Sara" />;
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Gallery</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
 }
