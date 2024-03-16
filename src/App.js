7a0f0ba93ba16afff7397cd36db0ff227715bcb2
Chris <who7708@163.com>
2024-03-17 01:48:29 +0800

    在 JSX 中通过大括号使用 JavaScript

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 // 导入Profile组件
 import Profile from "./Profile";
-import TodoList from "./TodoList";
+// import TodoList from "./TodoList";
 
-// 导出Gallery组件
-export default function Gallery() {
+const person = {
+  name: "John Doe",
+  age: 30,
+  city: "New York",
+  country: "USA",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  skills: ["JavaScript", "React", "Node", "Express"],
+  social: {
+    facebook: "https://www.facebook.com/",
+  },
+  theme: {
+    color: "red",
+    background: "black",
+  },
+};
+
+// // 导出Gallery组件
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Gallery</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//       <TodoList todos={[]} />
+//     </section>
+//   );
+// }
+
+export default function TodoList() {
   return (
-    <section>
-      <h1>Gallery</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-      <TodoList todos={[]} />
-    </section>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
   );
 }
